ca6168e5e8a1ac0854dcdaf8e1e0656efa8706a0
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-09-08 23:07:21 +0530

    Create First_Program.py

---
+++ Python Codes Yt/First_Program.py
@@ -0,0 +1,24 @@
+name = "sai"
+age = 17
+price = 25.33
+old = True
+a = None
+b = 10
+c = 20
+print("my name is : ", name)    #my name is : sai
+print("my age is : ", age)      #my age is : 17
+print(type(name))       # str(string)
+print(type(age))         #int(integer)
+print(type(price))     #float(decimals)
+print(type(old))      #bool(boolean)
+print(type(a))      #None type
+print(b == c)       #false
+print(b != c)     #true
+print(b % c)    # remainder
+print(b % b)
+print(b ** c)    #b^c
+
+
+num = 10
+num = num + 10  # it can also be written as  [num += 10]
+print(num)    # 10+10 =>20
