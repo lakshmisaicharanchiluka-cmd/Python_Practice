c02b43a6c96f2ec1a8c20e71a8783d24ed677083
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-09-09 21:42:43 +0530

    Create question_4.py

---
+++ Python Codes Yt/questions.py/question_4.py
@@ -0,0 +1,5 @@
+#WAP to input 2 int numbers a and b  print true if a is greater than or equal to b if not print false
+a = int(input("enter first number a  :  "))
+b = int(input("enter secont number b  :  "))
+ 
+print(a>=b)
