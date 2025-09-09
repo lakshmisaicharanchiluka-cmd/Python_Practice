08118a79c6fff61eb3fb0cc80153325aef47b2a3
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-09-09 20:24:10 +0530

    Create input.py

---
+++ Python Codes Yt/input.py
@@ -0,0 +1,11 @@
+name = input("Enter Your Name  :")
+
+print("welcome", name)
+
+no = int(input("enter your number  :"))
+print("your entered phone number is :" , no)
+
+s
+mark = float(input("enter your marks :"))
+
+print("your entered marks are  :" , mark)  
