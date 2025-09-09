3edd71f3d02dbe70a18b45831fcda68d745ecfe5
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-09-09 20:45:07 +0530

    added 2 files

--- Python Codes Yt/input.py
+++ Python Codes Yt/input.py
@@ -1,11 +1,25 @@
-name = input("Enter Your Name  :")
+#name = input("Enter Your Name  :")
 
-print("welcome", name)
+#print("welcome", name)
 
-no = int(input("enter your number  :"))
-print("your entered phone number is :" , no)
+#no = int(input("enter your number  :"))
+#print("your entered phone number is :" , no)
 
-s
-mark = float(input("enter your marks :"))
+#mark = float(input("enter your marks :"))
 
-print("your entered marks are  :" , mark)  
+#print("your entered marks are  :" , mark)  
+
+
+
+
+
+val = input("enter")
+print(type(val))
+
+val2 = int(input("enter  :")) 
+print(type(val2))
+
+val3 = float(input("enter  :"))
+
+print(type(val3))
+print(val3)
