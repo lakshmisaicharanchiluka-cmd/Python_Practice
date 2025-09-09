d5ad2de763e64cf62643aaa660984562596df2b2
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-09-09 21:32:27 +0530

    added

---
+++ Python Codes Yt/questions.py/question_3.py
@@ -0,0 +1,6 @@
+#WAP to inpute 2 floating point numbers and print their average
+
+a = float(input("enter first number   : "))
+b = float(input("enter second number   : "))
+
+print("average = ",(a+b)/2)
