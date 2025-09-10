f80134cc8fbfae012a4179814ca817b0ec78c011
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-09-10 20:21:50 +0530

    Update question_7.py

--- Python Codes Yt/questions.py/question_7.py
+++ Python Codes Yt/questions.py/question_7.py
@@ -1,10 +1,12 @@
-marks = 80
-if(marks >= 90):
+marks = int(input("Enter students Marks  :  "))
+if(marks >= 90 and marks<=100):
     grade = "A"
 elif(marks >= 80 and marks<90):
     grade = "B"
 elif(marks >=70 and marks<80):
     grade = "C"
+elif(marks >100):
+    grade = "invalide"
 else:
     grade = "d"
 
