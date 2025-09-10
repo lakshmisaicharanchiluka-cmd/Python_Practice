3f38dccaf81cbf01cb513d951f9ae6c5630ba5ad
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-09-10 20:13:14 +0530

    Create question_7.py

---
+++ Python Codes Yt/questions.py/question_7.py
@@ -0,0 +1,11 @@
+marks = 80
+if(marks >= 90):
+    grade = "A"
+elif(marks >= 80 and marks<90):
+    grade = "B"
+elif(marks >=70 and marks<80):
+    grade = "C"
+else:
+    grade = "d"
+
+print("Grade Of The Student  ->  ", grade)
