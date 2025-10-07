f541715d46c0dd5857f40c6fbd6b6abd21e54b47
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-10-07 21:19:08 +0530

    Update practice9.py

--- Python Codes Yt/practice/practice9.py
+++ Python Codes Yt/practice/practice9.py
@@ -1,6 +1,16 @@
-count = 1
+
+'''count = 1
 while count<= 5 :
     print("Hello")
     count += 1
     
-print(count)
+print(count)'''
+
+         #or
+
+i = 1
+
+while i <= 10:
+    print("Swetha")
+    i += 1
+print(i)
