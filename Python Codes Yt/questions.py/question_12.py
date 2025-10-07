f26ea05921d25252ced08fff662aaa780e12ff5f
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-10-07 19:26:44 +0530

    palindrome

---
+++ Python Codes Yt/questions.py/question_12.py
@@ -0,0 +1,13 @@
+list1 = [ 1, 5, 1]
+list2 = [5, 6, 8, 7]
+
+
+copy_list1 = list1.copy()
+copy_list1.reverse()
+
+if(copy_list1  ==  list1):
+    print("It Is A Palindrome")
+else:
+    print("It Is Not A Palindrome")
+
+
