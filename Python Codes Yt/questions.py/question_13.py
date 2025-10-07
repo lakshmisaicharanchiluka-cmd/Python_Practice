7d1b31d60864727d949f309d6f8005be160e2f71
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-10-07 19:35:58 +0530

    Create question_13.py

---
+++ Python Codes Yt/questions.py/question_13.py
@@ -0,0 +1,6 @@
+'''write a program to count the number of students with A grade in the following tuple
+("C" , "D" , "A" , "A" , "B" , "B" , "A")'''
+
+grade = ("C" , "D" , "A" , "A" , "B" , "B" , "A")
+
+print(grade.count("A"))
