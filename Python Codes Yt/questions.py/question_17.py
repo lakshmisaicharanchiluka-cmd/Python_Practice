7ff7770742d878bdfbb0edc1edaaef782e745cf7
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-10-14 19:35:55 +0530

    Create question_17.py

---
+++ Python Codes Yt/questions.py/question_17.py
@@ -0,0 +1,13 @@
+# write a programe to print the elements of the following list using a loop
+#[ 1 , 4 , 16 , 25 , 36 , 49 , 64 , 81 , 100 ]
+
+'''i = 1
+while i <= 10:
+    print(( i * i))
+    i += 1'''
+
+num = [ 1 , 4 , 16 , 25 , 36 , 49 , 64 , 81 , 100 ]
+i = 0
+while i < len ( num ):
+    print(num[i])    # num[0] , num[1] ..... 
+    i += 1
