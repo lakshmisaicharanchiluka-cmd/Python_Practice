96bfea6649b8265a09502fab8a427924d44258a3
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-10-14 19:06:59 +0530

    Create question_15.py

---
+++ Python Codes Yt/questions.py/question_15.py
@@ -0,0 +1,16 @@
+#  write a programe to print numbers from 1 to 100
+
+'''i = 1
+while i <= 100:
+    print(i)
+    i += 1
+print("Loop Ended")'''
+
+# write a programe to print numbers from 100 to 1
+
+i = 100
+while i >= 1:
+    print(i)
+    i -= 1
+
+print("Loop Ended")
