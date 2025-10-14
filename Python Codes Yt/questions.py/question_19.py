d80a0f306d0e381144ea40bec3db92aefa15cfc2
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-10-14 20:58:28 +0530

    Create question_19.py

---
+++ Python Codes Yt/questions.py/question_19.py
@@ -0,0 +1,10 @@
+# write a programe to print numbers from 1 to 100 using for and range
+
+'''for i in range(101):
+    print(i)'''
+
+
+# write a programe to print numbers from 100 to 1 using for and range
+
+for i in range(100,0, -1):
+    print(i)
