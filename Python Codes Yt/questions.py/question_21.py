fd9d2c619ba307961cd4adb6e9afe00cb6e1b79f
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-10-14 21:23:26 +0530

    Update question_21.py

--- Python Codes Yt/questions.py/question_21.py
+++ Python Codes Yt/questions.py/question_21.py
@@ -1,9 +1,18 @@
 # write a programe to find sum of first n natural numbers using while
 
 
-n = 5
+'''n = 5
 sum = 0
 for i in range(n + 1):
     sum += i
 
-print(" Total Sum = " , sum)
+print(" Total Sum = " , sum)'''
+
+
+n = 7
+sum = 0
+i = 1
+while (i <= n):
+    sum += i
+    i += 1
+print("Total Sum  =  " , sum)
