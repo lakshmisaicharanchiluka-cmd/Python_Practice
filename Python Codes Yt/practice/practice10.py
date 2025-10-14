bceab9282bdbff17d46da1056ea73ad21eaee88f
lakshmisaicharanchiluka-cmd <lakshmisaicharan.chiluka@gmail.com>
2025-10-14 20:28:48 +0530

    Update practice10.py

--- Python Codes Yt/practice/practice10.py
+++ Python Codes Yt/practice/practice10.py
@@ -1,7 +1,25 @@
-i = 0
+"""i = 0
 while i <= 5:
     if(i == 3):
-        i += 1
+        i += 1           
+        continue
+    print(i)
+    i += 1"""
+
+'''i = 1
+while i <= 10:
+    if(i % 2 == 0):
+        i += 1   # to print odd numbers        
+        continue
+    print(i)
+    i += 1'''
+
+
+
+i = 0
+while i <= 5:
+    if(i % 2 != 0):
+        i += 1        # to print even numbers
         continue
     print(i)
     i += 1
