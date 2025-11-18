792723ec587aa01e7ca4ab9d071b511820c9b5c4
lakshmisaicharanchiluka-cmd <228892460+lakshmisaicharanchiluka-cmd@users.noreply.github.com>
2025-11-18 19:29:34 +0530

    Update practice15.py

--- Python Codes Yt/practice/practice15.py
+++ Python Codes Yt/practice/practice15.py
@@ -1,12 +1,12 @@
 #remove the given word in all the places in a string 
 
-'''
+
 word = input("Enter A String   :")
 
 remove_word = input("Enter Removable Word    :")
 
-print(word.replace(remove_word,""))'''
+print(word.replace(remove_word,""))
 
-word = input("Enter A String   :")
+'''word = input("Enter A String   :")
 
-print(word.replace("hi", "Hello"))
+print(word.replace("hi", "Hello"))'''
